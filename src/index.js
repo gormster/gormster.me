485198b4ebec34d0675cbe54128ee33491ee6399
Morgan Harris <gormster@me.com>
2021-08-29 17:14:25 +1000

    Fix spacing

--- src/index.js
+++ src/index.js
@@ -6,42 +6,42 @@ customElements.define('email-button', EmailButton);
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    const helloBanner = $('#hello-banner')[0];
-    const helloLead = $('#hello-lead')[0];
-    const helloImage = $('#hello-image')[0];
-    const main = $('main')[0];
+  const helloBanner = $('#hello-banner')[0];
+  const helloLead = $('#hello-lead')[0];
+  const helloImage = $('#hello-image')[0];
+  const main = $('main')[0];
 
-    function hashchange() {
-        if (!window.location.hash) {
-            window.location.hash = '#ios';
-        }
-
-        let target = $(window.location.hash)[0];
-
-        $('.nav-link').forEach( el => {
-            el.classList.remove('active')
-            el.ariaSelected = false;
-        });
-
-        let tab = $(`.nav-link[href="${window.location.hash}"]`)[0];
-        tab.classList.add('active');
-        tab.ariaSelected = true;
-
-        helloBanner.textContent = target.dataset.helloBanner;
-        helloLead.textContent = target.dataset.helloLead;
-        helloImage.src = target.dataset.helloImage;
-
-        let removeThese = [].filter.call(main.classList, (t) => t.match(/tab-/));
-        main.classList.remove(...removeThese);
-        main.classList.add(`tab-${tab.dataset.tabId}`);
+  function hashchange() {
+    if (!window.location.hash) {
+      window.location.hash = '#ios';
     }
 
-    window.onhashchange = hashchange;
+    let target = $(window.location.hash)[0];
 
-    hashchange();
-
-    window.addEventListener('load', () => {
-        document.body.classList.remove('preload');
+    $('.nav-link').forEach( el => {
+      el.classList.remove('active')
+      el.ariaSelected = false;
     });
+
+    let tab = $(`.nav-link[href="${window.location.hash}"]`)[0];
+    tab.classList.add('active');
+    tab.ariaSelected = true;
+
+    helloBanner.textContent = target.dataset.helloBanner;
+    helloLead.textContent = target.dataset.helloLead;
+    helloImage.src = target.dataset.helloImage;
+
+    let removeThese = [].filter.call(main.classList, (t) => t.match(/tab-/));
+    main.classList.remove(...removeThese);
+    main.classList.add(`tab-${tab.dataset.tabId}`);
+  }
+
+  window.onhashchange = hashchange;
+
+  hashchange();
+
+  window.addEventListener('load', () => {
+    document.body.classList.remove('preload');
+  });
 });
 
